c213f17cf010367868fb2644b70197d11470180c
devdilshan2025 <dsbandara4@gmail.com>
2026-04-10 16:40:45 +0530

    Add User dto class

---
+++ src/main/java/edu/icet/model/dto/User.java
@@ -0,0 +1,26 @@
+package edu.icet.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class User {
+
+    private Long userId;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @NotBlank(message = "Role is required")
+    private String role;
+}
