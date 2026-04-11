863b1644b840e4910c062c65f1be642d97b9318d
devdilshan2025 <dsbandara4@gmail.com>
2026-04-11 17:55:53 +0530

    Update permit All configuration

--- src/main/java/edu/icet/config/SecurityConfig.java
+++ src/main/java/edu/icet/config/SecurityConfig.java
@@ -1,13 +1,32 @@
+package edu.icet.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll() // Login/Register හැමෝටම
+                        .requestMatchers(HttpMethod.GET, "/api/v1/car/get-all").permitAll() // කාර් බලන්න හැමෝටම
+                        .anyRequest().authenticated() // අනිත් හැම දේකටම Token එක ඕනේ
+                );
+        return http.build();
     }
 }
