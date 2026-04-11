6f716237c517ddf5aae8e66b34f08dc57ac05d57
devdilshan2025 <dsbandara4@gmail.com>
2026-04-11 17:52:55 +0530

    Add AuthController class and configuration

---
+++ src/main/java/edu/icet/controller/AuthController.java
@@ -0,0 +1,30 @@
+package edu.icet.controller;
+
+import edu.icet.model.dto.AuthResponseDTO;
+import edu.icet.model.dto.User;
+import edu.icet.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+@CrossOrigin
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody User userDTO) {
+        String response = authService.register(userDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody User loginRequest) {
+        AuthResponseDTO response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
+}
