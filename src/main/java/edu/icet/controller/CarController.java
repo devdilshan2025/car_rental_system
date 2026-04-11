9a3c2134faaa5d9a7367e71dabf6f86b3a6e2ddc
devdilshan2025 <dsbandara4@gmail.com>
2026-04-11 22:40:15 +0530

    Add car controller class

---
+++ src/main/java/edu/icet/controller/CarController.java
@@ -0,0 +1,51 @@
+package edu.icet.controller;
+
+import edu.icet.model.dto.Car;
+import edu.icet.service.CarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/car")
+@RequiredArgsConstructor
+@CrossOrigin
+public class CarController {
+
+    private final CarService carService;
+
+
+    @GetMapping("/get-all")
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addCar(@RequestBody Car carDTO) {
+        carService.addCar(carDTO);
+        return ResponseEntity.ok("Car Added Successfully!");
+    }
+
+
+    @GetMapping("/get/{id}")
+    public Car getCarById(@PathVariable Integer id) {
+        return carService.getCarById(id);
+    }
+
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateCar(@RequestBody Car carDTO) {
+        carService.updateCar(carDTO);
+        return ResponseEntity.ok("Car Updated Successfully!");
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteCar(@PathVariable Integer id) {
+        carService.deleteCar(id);
+        return ResponseEntity.ok("Car Deleted Successfully!");
+    }
+}
