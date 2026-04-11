c72ee808e4faad23cb17a9b659aa8fc51dc577c0
devdilshan2025 <dsbandara4@gmail.com>
2026-04-11 17:54:59 +0530

    Add model mapper configuration

--- src/main/java/edu/icet/Main.java
+++ src/main/java/edu/icet/Main.java
@@ -1,7 +1,9 @@
 package edu.icet;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
@@ -9,5 +11,9 @@ public class Main {
 
         SpringApplication.run(Main.class);
 
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
