8bbb13851bcdc28506531ef53a93341569bb5df3
devdilshan2025 <dsbandara4@gmail.com>
2026-04-12 11:05:15 +0530

    Add sorting query for car repository class

--- src/main/java/edu/icet/repository/CarRepository.java
+++ src/main/java/edu/icet/repository/CarRepository.java
@@ -4,7 +4,11 @@ import edu.icet.model.entity.CarEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<CarEntity, Integer> {
+
+    List<CarEntity> findAllByOrderByCarIdAsc();
 
 }
