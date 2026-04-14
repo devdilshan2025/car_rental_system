dcc2dec964a78c736083bdaa364bbd43555f8251
devdilshan2025 <dsbandara4@gmail.com>
2026-04-14 18:10:18 +0530

    Update AuthResponseDTO

--- src/main/java/edu/icet/model/dto/AuthResponseDTO.java
+++ src/main/java/edu/icet/model/dto/AuthResponseDTO.java
@@ -14,5 +14,5 @@ public class AuthResponseDTO {
     private String name;
     private String email;
     private String role;
-    private Long userId; // ඔයාගේ User DTO එකේ වගේම මෙතනත් Long පාවිච්චි කරන්න
+    private Long userId;
 }
