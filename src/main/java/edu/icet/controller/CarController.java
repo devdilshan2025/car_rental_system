496807b478d30f0554a352f2aa1e13df44152b29
devdilshan2025 <dsbandara4@gmail.com>
2026-04-14 18:05:45 +0530

    Crossorigin changed of carController

--- src/main/java/edu/icet/controller/CarController.java
+++ src/main/java/edu/icet/controller/CarController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/car")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CarController {
 
     private final CarService carService;
