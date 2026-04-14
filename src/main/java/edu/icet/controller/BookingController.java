1fb7e920051cb8aa37b61549987c0b989993eec3
devdilshan2025 <dsbandara4@gmail.com>
2026-04-14 18:06:14 +0530

    Crossorigin changed of BookingController

--- src/main/java/edu/icet/controller/BookingController.java
+++ src/main/java/edu/icet/controller/BookingController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/booking")
 @RequiredArgsConstructor
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:5173")
 public class BookingController {
 
     private final BookingService bookingService;
